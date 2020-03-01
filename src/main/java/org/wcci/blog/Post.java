de9f5f38922d00e41df56497fc78c61db7f0086a
brandonroth02 <59844743+brandonroth02@users.noreply.github.com>
2020-03-01 12:35:13 -0500

    finished post controller class

--- src/main/java/org/wcci/blog/Post.java
+++ src/main/java/org/wcci/blog/Post.java
@@ -33,4 +33,68 @@ public class Post {
     public Collection<Tag> getTags() {return tags; }
 
     public String getPostName() { return postName;}
+
+    public String getPostAuthor() {return postAuthor;}
+
+    public Date getPostDate() {return postDate;}
+
+    public String getPostText() {return postText;}
+
+    public Category getPostCategory() {return postCategory;}
+
+    public Long getId() {return id;}
+
+    public void addTag(Tag tagToAdd) {tags.add(tagToAdd);}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return Objects.equals(postName, post.postName) &&
+                Objects.equals(postCategory, post.postCategory) &&
+                Objects.equals(postAuthor, post.postAuthor) &&
+                Objects.equals(postDate, post.postDate) &&
+                Objects.equals(postText, post.postText) &&
+                Objects.equals(id, post.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return  Objects.hash(postAuthor, postDate, postText, postCategory, postName, id, tags);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
--- src/main/java/org/wcci/blog/PostController.java
+++ src/main/java/org/wcci/blog/PostController.java
@@ -1,4 +1,46 @@
 package org.wcci.blog;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.Optional;
+
+@Controller
 public class PostController {
+
+    private PostStorage postStorage;
+    private CategoryStorage categoryStorage;
+    private TagRepository tagRepository;
+
+    public PostController(PostStorage postStorage, CategoryStorage categoryStorage, TagRepository tagRepository){
+        this.postStorage = postStorage;
+        this.categoryStorage = categoryStorage;
+        this.tagRepository = tagRepository;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
